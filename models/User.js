39de0fab19ae27180b0446f80227b4235c1dea21
mostafa93sh <mostafa.khalel.93@gmail.com>
2026-05-02 02:04:07 +0300

    feat(user): add role and update email validation
    - Add role field to user schema
    - Implement `admin` and `user` roles
    - Update email validation logic
    - Use `validator` library for email

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -10,16 +11,21 @@ const UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, "Please provide email"],
-    match: [
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      "Please provide a valid email",
-    ],
+    validate: {
+      validator: validator.isEmail,
+      message: "Please Provide valid email",
+    },
     unique: true,
   },
   password: {
     type: String,
     required: [true, "Please provide password"],
     minlength: 6,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user",
   },
 });
 
