0681ca80a17faba8527eccd159e68f5c1206b379
mostafa93sh <mostafa.khalel.93@gmail.com>
2026-05-02 01:17:32 +0300

    feat(db): integrate database connection
    - Add connectDB import
    - Refactor startup into async function
    - Call connectDB before app.listen
    - Implement try/catch for connection

--- app.js
+++ app.js
@@ -2,9 +2,20 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
+// connectDB
+const connectDB = require("./db/connect");
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(PORT, () =>
+      console.log(`Server is listening on port ${PORT}...`),
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
