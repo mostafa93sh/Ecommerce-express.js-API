2e29a81ab90672cbaa02af8ae6efde781f73d59c
mostafa93sh <mostafa.khalel.93@gmail.com>
2026-05-02 01:09:26 +0300

    feat(server): initialize Express app
    - Initialize Express application
    - Load environment variables
    - Start server on configured port
    - Log server startup message

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('E-Commerce API');
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
