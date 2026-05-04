f0ad40e63af2142e0b7a5a165d8b49993314c86b
mostafa93sh <mostafa.khalel.93@gmail.com>
2026-05-04 15:46:21 +0300

    feat(auth): implement user registration
    - Validate unique email
    - Assign admin role to first user
    - Create new user in database
    - Return new user details

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,23 @@
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
+const { attachCookiesToResponse, createTokenUser } = require("../utils");
 const register = async (req, res) => {
-  res.send("regiter work");
+  const { email, name, password } = req.body;
+
+  const emailAlreadyExists = await User.findOne({ email });
+  if (emailAlreadyExists) {
+    throw new CustomError.BadRequestError("Email already exists");
+  }
+
+  // first registered user is an admin
+  const isFirstAccount = (await User.countDocuments({})) === 0;
+  const role = isFirstAccount ? "admin" : "user";
+
+  const user = await User.create({ name, email, password, role });
+
+  // attachCookiesToResponse({ res, user: createTokenUser(user) });
+  res.status(StatusCodes.CREATED).json({ user: createTokenUser(user) });
 };
 const login = async (req, res) => {
   res.send("login work");
