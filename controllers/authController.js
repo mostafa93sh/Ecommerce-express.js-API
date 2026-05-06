aa6ce92136881caba8ee72bc8e0b940ee93e16b7
mostafa93sh <mostafa.khalel.93@gmail.com>
2026-05-06 09:31:19 +0300

    feat(auth): implement login and logout
    - Implement full login functionality
    - Implement full logout functionality
    - Refine token handling in registration
    - Add input validation for login
    - Attach auth cookies

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,15 +15,37 @@ const register = async (req, res) => {
   const role = isFirstAccount ? "admin" : "user";
 
   const user = await User.create({ name, email, password, role });
+  const tokenUser = { name: user.name, userId: user._id, role: user.role };
 
-  // attachCookiesToResponse({ res, user: createTokenUser(user) });
-  res.status(StatusCodes.CREATED).json({ user: createTokenUser(user) });
+  attachCookiesToResponse({ res, user: tokenUser });
+  res.status(StatusCodes.CREATED).json({ user: tokenUser });
 };
+
+// login 
 const login = async (req, res) => {
-  res.send("login work");
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new CustomError.BadRequestError("Please provide email and password");
+  }
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new CustomError.UnauthenticatedError("Invalid Credentials");
+  }
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new CustomError.UnauthenticatedError("Invalid Credentials");
+  }
+  const tokenUser = { name: user.name, userId: user._id, role: user.role };
+  attachCookiesToResponse({ res, user: tokenUser });
+  res.status(StatusCodes.OK).json({ user: tokenUser });
 };
 const logout = async (req, res) => {
-  res.send("logout work");
+  res.cookie("token" , "logout", {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  });
+  res.status(StatusCodes.OK).json({ msg: "User logged out successfully" });
 };
 
 module.exports = {
