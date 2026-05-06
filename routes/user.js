1b2494234381055a96763288faadc21c22c00cc2
mostafa93sh <mostafa.khalel.93@gmail.com>
2026-05-06 09:29:28 +0300

    feat(user): add initial user routes
    - Create routes/user.js file
    - Set up Express router for user paths
    - Register GET / route for all users
    - Import

---
+++ routes/user.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getAllUsers,
+  getSingleUser,
+  showCurrentUser,
+  updateUser,
+  updateUserPassword,
+} = require("../controllers/userController");
+
+router.get("/", getAllUsers);
+
+module.exports = router;
